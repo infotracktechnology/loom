efbb5fea26fccc7ad44baa8a022203ce6eceb70e
infotracktechnology <infotracktechnologysolutions@gmail.com>
2023-08-18 16:47:03 +0530

    New update

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set('Asia/Kolkata');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/loom/';
+$config['base_url'] = 'https://loom.mwheelers.com/';
 
 /*
 |--------------------------------------------------------------------------
