5fb5712f5a43c4299d49d76fc902b4b5ac4f1290
infotracktechnology <infotracktechnologysolutions@gmail.com>
2023-09-18 16:46:08 +0530

    updated

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'AuthController/index';
 $route['404_override'] = '';
+$route['production']['GET'] = "ProductionController/index";
 //$route['login'] = "AuthController/index";
 $route['translate_uri_dashes'] = FALSE;
---
+++ application/views/production.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Loom - Admin Dashboard  </title>
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/app.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/components.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/custom.css">
+  <link rel='shortcut icon' type='image/x-icon' href='<?php echo base_url();?>assets/img/favicon.ico' />
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bundles/datatables/datatables.min.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
+</head>
+
+<body>
+  <div class="loader"></div>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+    <?php include_once 'sidebar.php';?>
+      <div class="main-content">
+ <section class="section">
+  <div class="section-body">
+     <div class="card card-primary">
+  <div class="card-body">
+     <form method="post" id="myForm" action="">
+
+     </form>
+
+  
+</div>
+</div>
+
+</div>
+</section>
+</div>
+</div>
+</div>
+
+  
+
+  
+
+  
+
+  <!-- Core JS -->
+<script src="<?php echo base_url();?>assets/js/app.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/page/index.js"></script>
+<script src="<?php echo base_url();?>assets/js/scripts.js"></script>
+<script src="<?php echo base_url();?>assets/js/custom.js"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<script src="<?php echo base_url();?>assets/bundles/datatables/datatables.min.js"></script>
+<script src="<?php echo base_url();?>assets/bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js"></script>
+
+
+</body>
+</html>
+
+<!-- / Content -->
+
