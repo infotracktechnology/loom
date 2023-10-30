b93c9e59250beb085a6ac8dc941aafcae72cc021
infotracktechnology <infotracktechnologysolutions@gmail.com>
2023-10-30 15:49:43 +0530

    ok

--- application/controllers/ProductionController.php
+++ application/controllers/ProductionController.php
@@ -21,7 +21,7 @@ class ProductionController extends CI_Controller {
     }
 
     public function loom($id){
-        $loom = $this->db->query("SELECT a.loom_id,c.id,c.size,c.pick,c.product_name,((1848*d.count*36)/(39.36*c.pick*52+2))grm,c.coolie FROM `loom_master` a left join loom_product b on a.loom_id=b.loom_id left join product_master c on b.product_id=c.id left join material_master d on c.weft_yarn=d.material_id WHERE a.loom_id=$id")->row();
+        $loom = $this->db->query("SELECT a.loom_id,c.id,c.size,c.pick,c.product_name,round((1848*d.count*36)/(39.36*c.pick*52+2),2)grm,c.coolie FROM `loom_master` a left join loom_product b on a.loom_id=b.loom_id left join product_master c on b.product_id=c.id left join material_master d on c.weft_yarn=d.material_id WHERE a.loom_id=$id")->row();
         $this->output->set_content_type('application/json')->set_output(json_encode($loom));
     }
 }
