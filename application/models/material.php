c491adbaa43e5aa4a2e30a716a33f13724daaa30
infotracktechnology <infotracktechnologysolutions@gmail.com>
2023-10-30 15:18:55 +0530

    pok

--- application/models/material.php
+++ application/models/material.php
@@ -1,5 +1,5 @@
 <?php
-class Material extends ci_model
+class Material extends CI_Model
 {
 	public function fetchMaterial($materialName, $groupField = 'count') {
         $query = $this->db->select('material_id,'.$groupField)->from('material_master')->where('material_name', $materialName)->group_by($groupField)->get();
