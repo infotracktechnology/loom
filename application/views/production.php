ea588e739c56c97e1e24f34cde9f4455fdda12d5
infotracktechnology <infotracktechnologysolutions@gmail.com>
2023-11-07 18:07:10 +0530

    ok

--- application/views/production.php
+++ application/views/production.php
@@ -35,7 +35,7 @@
    
     </div>
   <div class="card-body">
-  <div class="col-12">
+  <div class="row">
                 <div class="table-responsive">
                 <table id="myTable" class="table table-striped table-bordered table-sm">
     
